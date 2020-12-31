230a334511ec19e192eff1c1fcc2e0f7f238d25d
aksmith5239 <aklobby@gmail.com>
2020-12-31 08:25:38 -0700

    users added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,14 @@ const { gql } = require('apollo-server-express');
 
 //create typeDefs
 const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+}
 type Thought {
     _id: ID
     thoughtText: String
@@ -19,7 +27,10 @@ type Reaction {
 }
 
 type Query {
+    users: [User]
+    user(username: String!): User
     thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
 }
 `;
 
